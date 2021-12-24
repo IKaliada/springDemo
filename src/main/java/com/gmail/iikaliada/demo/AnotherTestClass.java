66934c3713805ee9dadeceeff6725a2d50ca1c3e
Ivan Kaliada <i.kaliada@eis-partners.com>
2021-12-24 12:53:00 +0100

    Add print ln lines

--- src/main/java/com/gmail/iikaliada/demo/AnotherTestClass.java
+++ src/main/java/com/gmail/iikaliada/demo/AnotherTestClass.java
@@ -3,5 +3,8 @@ package com.gmail.iikaliada.demo;
 public class AnotherTestClass {
     public void someMethod(){
         System.out.println("Some text");
+        System.out.println("Some text");
+        System.out.println("Some text");
+        System.out.println("Some text");
     }
 }
