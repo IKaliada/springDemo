edb04e01f4599ff4b739744c693c374b50b568f2
Ivan Kaliada <i.kaliada@eis-partners.com>
2021-12-29 12:15:57 +0100

    Add 2 lines

--- src/main/java/com/gmail/iikaliada/demo/TestClass.java
+++ src/main/java/com/gmail/iikaliada/demo/TestClass.java
@@ -4,5 +4,7 @@ public class TestClass {
 
     public void showSomething() {
         System.out.println("Something wrong");
+        System.out.println("Something bad");
+        System.out.println("Something good");
     }
 }
