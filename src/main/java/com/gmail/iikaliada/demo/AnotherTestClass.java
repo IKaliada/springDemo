5e8b870d2dd85ed49a1b047af10ecc01d5538351
Ivan Kaliada <i.kaliada@eis-partners.com>
2022-02-09 14:58:36 +0100

    Add some lines

--- src/main/java/com/gmail/iikaliada/demo/AnotherTestClass.java
+++ src/main/java/com/gmail/iikaliada/demo/AnotherTestClass.java
@@ -4,5 +4,9 @@ public class AnotherTestClass {
     public void someMethod(){
         System.out.println("Some text");
         System.out.println("Some text");
+        System.out.println("Some text");
+        System.out.println("Some text");
+        System.out.println("Some text");
+        System.out.println("Some text");
     }
 }
